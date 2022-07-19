8d3b750cd0b3552c059fe86134fd251a65933d65
Meher <mehersinghvasir@gmail.com>
2022-07-19 21:37:34 +0530

    Complete Iteration 0

--- To-Do List/ToDoTableViewController.swift
+++ To-Do List/ToDoTableViewController.swift
@@ -8,38 +8,58 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
-
+    var name = ""
+    var important = false
+    
+    var toDos : [ToDo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+          toDos = createToDos()
+        }
+    
+    
+    func createToDos() -> [ToDo] {
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+
+      return [swift, dog]
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return toDos.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+          } else {
+            cell.textLabel?.text = toDo.name
+          }
 
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
